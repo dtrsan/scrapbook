2802514ed80e08088dccdd5b6594071aefd9410c
Domagoj Tršan <domagoj.trsan@gmail.com>
2012-09-10 21:07:21 +0200

    git: add git configuration

---
+++ git/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NAMESPACE="git"
+FILES=( gitconfig )
+
+if [ "$DIR" == "" ]; then
+    DIR="$( cd -P "$( dirname "$0" )" && pwd )"
+fi
+
+for i in ${FILES[@]}; do
+    echo -e "\033[1;32m-> installing \033[1;37m$NAMESPACE/$i \033[1;32mto \033[1;37m~/.$i\033[0m"
+    /usr/bin/install -m 600 $DIR/$i ~/.$i
+done
+
