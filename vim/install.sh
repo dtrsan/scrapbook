4d70bb47e99fd64ac82f0fb39c73aba73a9aa69c
Domagoj Tršan <domagoj.trsan@gmail.com>
2014-12-20 13:46:12 +0100

    vim: add solarize colorscheme

--- vim/install.sh
+++ vim/install.sh
@@ -2,6 +2,8 @@
 
 NAMESPACE="vim"
 FILES=( vimrc )
+COLORSCHEMES=( solarized.vim )
+COLORSCHEMES_DIR=~/.vim/colors
 
 if [ "$DIR" == "" ]; then
     DIR="$( cd -P "$( dirname "$0" )" && pwd )"
@@ -10,5 +12,14 @@ fi
 for i in ${FILES[@]}; do
     echo -e "\033[1;32m-> installing \033[1;37m$NAMESPACE/$i \033[1;32mto \033[1;37m~/.$i\033[0m"
     /usr/bin/install -m 644 $DIR/$i ~/.$i
+done
+
+if [ ! -d "$COLORSCHEMES_DIR" ]; then
+    mkdir -p "$COLORSCHEMES_DIR"
+fi
+
+for i in ${COLORSCHEMES[@]}; do
+    echo -e "\033[1;32m-> installing \033[1;37m$NAMESPACE/$i \033[1;32mto \033[1;37m~/.vim/colors/$i\033[0m"
+    /usr/bin/install -m 644 $DIR/$i ~/.vim/colors/$i
 done
 
