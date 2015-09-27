cfe1a3d790c6efd39a84bf8396ecafd90e8d6d6c
Domagoj Tršan <domagoj.trsan@gmail.com>
2015-09-27 18:58:59 +0100

    tmux: plugins: add tpm

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,6 +3,14 @@
 NAMESPACE="tmux"
 FILES=( tmux.conf )
 
+TPM_DIR='.tmux/plugins/tpm'
+TPM_URL='git://github.com/tmux-plugins/tpm'
+
+if [ ! -d "/$HOME/$TPM_DIR" ]; then
+    echo -e "\033[1;32m-> installing \033[1;37mtpm \033[1;32mto \033[1;37m~/$TPM_DIR\033[0m"
+    git clone $TPM_URL ~/$TPM_DIR
+fi
+
 if [ "$DIR" == "" ]; then
     DIR="$( cd -P "$( dirname "$0" )" && pwd )"
 fi
